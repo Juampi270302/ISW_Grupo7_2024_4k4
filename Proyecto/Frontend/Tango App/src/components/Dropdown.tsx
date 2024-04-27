5c8aca89d9ce75c77e2f5028a6bffe5a7253ba14
MarianoVigliocco <nvigliocco@gmail.com>
2024-04-27 13:45:02 -0300

    Renderizado de componentes de pago

--- Proyecto/Frontend/Tango App/src/components/Dropdown.tsx
+++ Proyecto/Frontend/Tango App/src/components/Dropdown.tsx
@@ -1,53 +1,58 @@
 import React, { useState } from 'react';
 import RNPickerSelect from 'react-native-picker-select';
-import { View, Text, StyleSheet} from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 import { FormaPago } from '@/utils/Types';
 
-interface PlaceHolderProps{
-    label: string
-    value: null
+interface PlaceHolderProps {
+    label: string;
+    value: null;
 }
 
-interface OptionsProps{
-    label: string
-    value: FormaPago
+interface OptionsProps {
+    label: string;
+    value: FormaPago;
 }
 
 interface DropdownProps {
     placeholder: PlaceHolderProps;
     options: OptionsProps[];
+    onSelectOption: (selectedOption: OptionsProps) => void;
 }
 
+export const Dropdown = ({ placeholder, options, onSelectOption }: DropdownProps) => {
+    const [selectedValue, setSelectedValue] = useState(null);
 
-
-export const Dropdown = ({placeholder, options}: DropdownProps) => {
-    const [selectedValue, setSelectedValue] = useState(null)
-    
-
-      return (
-        <View style={styles.button}>
-          <Text>Select an option:</Text>
-          <RNPickerSelect 
-            placeholder={placeholder}
-            items={options}
-            onValueChange={(value) => setSelectedValue(value)}
-            value={selectedValue}
-          />
-        </View>
-      );
+    const handleSelectOption = (value) => {
+        if (value !== null) { // Ignorar la selección del marcador de posición
+            const selectedOption = options.find(option => option.value === value);
+            onSelectOption(selectedOption); // Llama a la función onSelectOption con la opción seleccionada
+            setSelectedValue(value);
+            // console.log(selectedValue.forma_pago)
+        }
     };
 
-    const styles = StyleSheet.create({
-        button: {
-            backgroundColor: '#fef9cc',
-            padding: 10,
-            borderRadius: 5,
-            alignItems: 'center',
-          },
-          buttonText: {
-            color: '#0f1b38',
-            fontSize: 16,
-          },
-    })
+    return (
+        <View style={styles.button}>
+            <Text>Select an option:</Text>
+            <RNPickerSelect
+                placeholder={placeholder}
+                items={options}
+                onValueChange={(value) => handleSelectOption(value)}
+                value={selectedValue}
+            />
+        </View>
+    );
+};
 
-
+const styles = StyleSheet.create({
+    button: {
+        backgroundColor: '#fef9cc',
+        padding: 10,
+        borderRadius: 5,
+        alignItems: 'center',
+    },
+    buttonText: {
+        color: '#0f1b38',
+        fontSize: 16,
+    },
+});
