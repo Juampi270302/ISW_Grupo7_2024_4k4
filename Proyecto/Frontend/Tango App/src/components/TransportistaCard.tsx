11f07ab24560cb93df53ef664374106cacf0c793
Juampi270302 <juampylambertucci2002@gmail.com>
2024-04-28 18:07:31 -0300

    Pasarela de pagos lista

--- Proyecto/Frontend/Tango App/src/components/TransportistaCard.tsx
+++ Proyecto/Frontend/Tango App/src/components/TransportistaCard.tsx
@@ -1,26 +1,27 @@
-import React, { View, Text, StyleSheet} from 'react-native'
+import React, {View, Text, StyleSheet} from 'react-native'
 
-import { useNavigation } from '@react-navigation/native'
+import {useNavigation} from '@react-navigation/native'
 
-import { Transportista, FormaPago} from '@/utils/Types'
-import { ButtonGood } from './ButtonGood'
+import {Transportista, TarjetaPago} from '@/utils/Types'
+import {ButtonGood} from './ButtonGood'
 
 interface TransportistaCardProps {
     transportista: Transportista
 }
 
-export const TransportistaCard = (props: TransportistaCardProps) =>{
+export const TransportistaCard = (props: TransportistaCardProps) => {
     const {nombre, calificacion, fecha_retiro, fecha_traslado, importe, forma_pago} = props.transportista
     const navigation = useNavigation()
 
-    const handleIngresarPress = () =>{
+    const handleIngresarPress = () => {
         console.log("Se ha apretado el boton")
-        navigation.navigate('Pago', {
+        let tarjetaPago:TarjetaPago = {
             formasPago: forma_pago,
             importe: importe,
             fecha_retiro: fecha_retiro,
             fecha_traslado: fecha_traslado
-        });
+        }
+        navigation.navigate('Pago', tarjetaPago);
     }
 
     return (
@@ -31,15 +32,21 @@ export const TransportistaCard = (props: TransportistaCardProps) =>{
             <Text>Fecha de traslado: {fecha_traslado}</Text>
             <Text>Importe: ${importe}</Text>
             <Text>Formas de pago: </Text>
-                {forma_pago.map((pago, index) => (
-            <Text key={index}>{pago.forma_pago}</Text>
+            {forma_pago.map((pago, index) => (
+                <Text key={index}>{pago.forma_pago}</Text>
             ))}
-        <ButtonGood title='Ingresar para pagar' onPress={handleIngresarPress} style={{button:{  backgroundColor: '#214E34',
-      padding: 10,
-      borderRadius: 20,
-      alignItems: 'center',
-      marginBottom: 20}, buttonText:{ color: 'white',
-      fontSize: 16,}}}  />
+            <ButtonGood title='Ingresar para pagar' onPress={handleIngresarPress} style={{
+                button: {
+                    backgroundColor: '#214E34',
+                    padding: 10,
+                    borderRadius: 20,
+                    alignItems: 'center',
+                    marginBottom: 20
+                }, buttonText: {
+                    color: 'white',
+                    fontSize: 16,
+                }
+            }}/>
         </View>
     )
 }
@@ -51,7 +58,7 @@ const styles = StyleSheet.create({
         borderRadius: 10,
         padding: 15,
         marginBottom: 15,
-        backgroundColor:'#DFF8EB'
+        backgroundColor: '#DFF8EB'
     },
     title: {
         fontSize: 18,
--- Proyecto/Frontend/Tango App/src/utils/Types.ts
+++ Proyecto/Frontend/Tango App/src/utils/Types.ts
@@ -8,8 +8,31 @@
     forma_pago: FormaPago[]
 }
 
+type TarjetaPago = {
+    formasPago: FormaPago[]
+    importe: number
+    fecha_retiro: string
+    fecha_traslado: string
+}
+
 type FormaPago = {
     forma_pago: string
 }
 
-export {Transportista, FormaPago}
+type DatosT = {
+    tipoTarjeta: string
+    numeroTarjeta: string
+    fechaVencimiento:string
+    pin:string
+    nombreCompleto: string
+    tipoDocumento:string
+    nroDocumento:string
+}
+
+type ProcesamientoPago = {
+    pagoExistoso:boolean
+    descripcion:string
+    codigoPago:number
+}
+
+export {Transportista, FormaPago, DatosT, ProcesamientoPago, TarjetaPago}
