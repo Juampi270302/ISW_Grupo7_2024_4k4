f5ccd796aa86efb7fa934208f01ef5fd8f08026c
MarianoVigliocco <nvigliocco@gmail.com>
2024-06-04 19:34:24 -0300

    Correccion en el funcionamiento del proyecto.

--- Proyecto/Frontend/Tango App/src/services/http.service.ts
+++ Proyecto/Frontend/Tango App/src/services/http.service.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const httpService = axios.create({
-    baseURL:"http://192.168.100.1:3000",
+    baseURL:"http://192.168.100.22:3000",
     responseType: "json",
     withCredentials:false
 });
